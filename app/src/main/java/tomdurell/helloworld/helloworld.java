ab46e438bc683fc61c78581472687529e14583b4
Computing <user@gmail.com>
2017-11-20 17:42:26 +0000

    First Workshop Complete

--- app/src/main/java/tomdurell/helloworld/helloworld.java
+++ app/src/main/java/tomdurell/helloworld/helloworld.java
@@ -3,6 +3,10 @@ package tomdurell.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// added for helloworld
+import android.view.View;
+import android.widget.*;
+
 public class helloworld extends AppCompatActivity {
 
     @Override
@@ -10,5 +14,14 @@ public class helloworld extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //interface layout file
         setContentView(R.layout.activity_helloworld);
+    }
+    /** Called when the user touches the button */
+    public void textChangeButton(View view)
+    {
+        // find the textview control to change
+        TextView TextView1 = (TextView)findViewById(R.id.changeText);
+        // set visibility to visible
+        TextView1.setVisibility(View.VISIBLE);
+
     }
 }
